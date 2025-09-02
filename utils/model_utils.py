263169a4550996944c1be2919ef4417cd6d0d981
Tanguy VANSNICK <191451@umons.ac.be>
2025-09-02 09:31:39 +0200

    fixing aggregation

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -3,16 +3,42 @@ import torch
 from collections import OrderedDict
 from typing import List
 
-def get_parameters(model):
-    # if we want to return only the optimized parameters
-    # return [val.detach().cpu().numpy() for name, val in model.named_parameters() if 'weight'  in name or 'bias' in name]
+def get_parameters(model, include_bn_stats=True):
+    """
+    Get ALL model parameters for federated aggregation.
+    
+    Args:
+        model: PyTorch model
+        include_bn_stats: Always True now - include everything except counters
+    """
+    # Simple approach: get ALL parameters except num_batches_tracked (it's a counter)
+    return [val.cpu().numpy() for name, val in model.state_dict().items() 
+            if 'num_batches_tracked' not in name]
 
-    # excluding parameters of BN layers when using FedBN
-    return [val.cpu().numpy() for name, val in model.state_dict().items() if 'bn' not in name]
-
-def set_parameters(model, parameters: List[np.ndarray]) -> None:
-    # Set model parameters from a list of NumPy ndarrays
-    keys = [k for k in model.state_dict().keys() if 'bn' not in k]
-    params_dict = zip(keys, parameters)
-    state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
-    model.load_state_dict(state_dict, strict=False)
+def set_parameters(model, parameters: List[np.ndarray], include_bn_stats=True) -> None:
+    """
+    Set ALL model parameters from federated aggregation.
+    
+    Args:
+        model: PyTorch model  
+        parameters: List of parameter arrays including BatchNorm stats
+        include_bn_stats: Always True now - expect everything except counters
+    """
+    # Get parameter names (exclude num_batches_tracked)
+    keys = [k for k in model.state_dict().keys() if 'num_batches_tracked' not in k]
+    
+    if len(keys) != len(parameters):
+        raise ValueError(f"Parameter count mismatch: model has {len(keys)} params, "
+                        f"received {len(parameters)}")
+    
+    # Build new state dict
+    new_state_dict = OrderedDict()
+    for name, param in zip(keys, parameters):
+        new_state_dict[name] = torch.tensor(param)
+    
+    # Keep num_batches_tracked from current model
+    for name in model.state_dict().keys():
+        if 'num_batches_tracked' in name:
+            new_state_dict[name] = model.state_dict()[name]
+    
+    model.load_state_dict(new_state_dict, strict=True)
